e04fd5362952bd2582f4d31da44aaf0cbb4ce182
Sayam Sethi <sayam.sethi2@gmail.com>
2019-12-04 19:41:20 +0530

    Late first commit with basic layout complete

---
+++ page.js
@@ -0,0 +1,9 @@
+var $root = $("html, body");
+
+$(document).on("click", "a[href^=\"#\"]", function (event) {
+    event.preventDefault();
+
+    $root.animate({
+        scrollTop: $($.attr(this, "href")).offset().top
+    }, 500);
+});
