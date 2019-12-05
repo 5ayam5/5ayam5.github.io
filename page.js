b09e713d675c016a786e8b40da640713d24e155e
Sayam Sethi <sayam.sethi2@gmail.com>
2019-12-05 16:35:41 +0530

    Working perfectly for only some aspect ratios.

--- page.js
+++ page.js
@@ -1,9 +1,38 @@
-var $root = $("html, body");
+window.onload = function () {
+    var $root = $("html, body");
+    window.div1Y = document.getElementById("div1").getBoundingClientRect().y;
+    window.div2Y = document.getElementById("div2").getBoundingClientRect().y;
+    window.div3Y = document.getElementById("div3").getBoundingClientRect().y;
+    window.commentsY = document.getElementById("comments").getBoundingClientRect().y;
+    window.socialY = document.getElementById("social").getBoundingClientRect().y;
 
-$(document).on("click", "a[href^=\"#\"]", function (event) {
-    event.preventDefault();
+    document.getElementById("ref_div1").addEventListener("click", function () {
+        $root.scrollTop(window.div1Y)
+    });
+    document.getElementById("ref_div2").addEventListener("click", function () {
+        $root.scrollTop(window.div2Y)
+    });
+    document.getElementById("ref_div3").addEventListener("click", function () {
+        $root.scrollTop(window.div3Y)
+    });
+    document.getElementById("ref_comments").addEventListener("click", function () {
+        $root.scrollTop(window.commentsY)
+    });
+    document.getElementById("ref_social").addEventListener("click", function () {
+        $root.scrollTop(window.socialY)
+    });
+    document.getElementById("up").addEventListener("click", function () {
+        $root.scrollTop(0)
+    });
+    document.getElementsByClassName("navbarhome")[0].addEventListener("click", function () {
+        $root.scrollTop(0)
+    });
+};
 
-    $root.animate({
-        scrollTop: $($.attr(this, "href")).offset().top
-    }, 500);
-});
+window.onresize = function () {
+    window.div1Y = document.getElementById("div1").getBoundingClientRect().y;
+    window.div2Y = document.getElementById("div2").getBoundingClientRect().y;
+    window.div3Y = document.getElementById("div3").getBoundingClientRect().y;
+    window.commentsY = document.getElementById("comments").getBoundingClientRect().y;
+    window.socialY = document.getElementById("social").getBoundingClientRect().y;    
+}
